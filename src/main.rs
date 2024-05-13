327fe1075a12a9e68faa61c54faec8ec66619cdc
wytsai7660 <wytsai7660@gmail.com>
2024-05-13 17:34:31 +0800

    Statements vs Expression

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+fn add(a: i32, b: i32) -> i32 {
+    return a + b;
+    // a + b
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n = 10;
+
+    let m = n > 5 ? 999 : 111;
+
+    // let m;
+    // if n > 5 {
+    //     m = 999;
+    // } else {
+    //     m = 111;
+    // }
+
+    // let m = if n > 5 { 999 } else { 111 };
+
+    // let m = match n {
+    //     0 => 1,
+    //     1 => 2,
+    //     2 => 3,
+    //     3 => 4,
+    //     _ => -1,
+    // };
+
+    println!("n = {}", n);
+    println!("m = {}", m);
+
+    add(n, m);
 }
