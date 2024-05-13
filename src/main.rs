d1f39b290e1a3837407314faa52be533dbd66c0e
wytsai7660 <wytsai7660@gmail.com>
2024-05-14 05:12:11 +0800

    box WIP

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,4 @@
 fn main() {
-    let reference;
-    {
-        let str = String::from("Hello, World!");
-        reference = &str;
-    }
-    print_but_dont_take(reference);
-
-    // let str = String::from("Hello, World!");
-    // take_and_return_ref(str);
+    let data = Box::new([5; 10]);
+    println!("{:?}", data);
 }
-
-#[allow(dead_code)]
-fn print_but_dont_take(str: &String) {
-    println!("{}", str);
-}
-
-// fn take_and_return_ref(str: String) -> &String {
-//     &str
-// }
