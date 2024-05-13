0775da67e814621bf60570173d9627a8bb5c2ab7
wytsai7660 <wytsai7660@gmail.com>
2024-05-14 03:00:42 +0800

    ownership basic

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,28 @@
 fn main() {
-    let mut str = String::from("Hello, World!");
-    // look_but_dont_take(&str);
+    let str = String::from("Hello, World!");
 
-    // let str2 = take_and_return(str);
-    append_but_dont_take(&mut str);
-
-    println!("{}", str);
-
+    let str2 = take_and_return(str);
     // println!("{}", str); // not owner anymore
-    // println!("{}", str2);
+    println!("{}", str2);
+
+    // print_but_dont_take(&str);
+    // println!("{}", str);
+
+    // modify_but_dont_take(&mut str);
+    // println!("{}", str);
 }
 
-// fn take_and_return(str: String) -> String {
-//     str
-// }
+#[allow(dead_code)]
+fn take_and_return(str: String) -> String {
+    str
+}
 
-// fn look_but_dont_take(str: &String) {
-//     println!("{}", str);
-// }
+#[allow(dead_code)]
+fn print_but_dont_take(str: &String) {
+    println!("{}", str);
+}
 
-fn append_but_dont_take(str: &mut String) {
+#[allow(dead_code)]
+fn modify_but_dont_take(str: &mut String) {
     str.push_str("!");
 }
