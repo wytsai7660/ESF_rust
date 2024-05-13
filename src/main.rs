23558035d0e81e300a83f3ae4006451bc26de296
wytsai7660 <wytsai7660@gmail.com>
2024-05-14 03:25:22 +0800

    dangling pointer

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,14 @@
 fn main() {
-    let mut str = String::from("Hello, World!");
-
-    let ref1 = &mut str;
-    let ref2 = &mut str;
-
-    modify_but_dont_take(ref1);
-    modify_but_dont_take(ref2);
-
-    // println!("{}", str);
-
-    // print_but_dont_take(ref1);
-    // print_but_dont_take(ref2);
+    // let ref: &mut String;
+    let reference;
+    {
+        let str = String::from("Hello, World!");
+        reference = &str;
+    }
+    print_but_dont_take(reference);
 }
 
 #[allow(dead_code)]
 fn print_but_dont_take(str: &String) {
     println!("{}", str);
-}
-
-#[allow(dead_code)]
-fn modify_but_dont_take(str: &mut String) {
-    str.push_str("!");
 }
